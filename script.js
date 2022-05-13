987a5677d815942f9218e2128e51aa46d21b0bc3
Subhan Khan <subhank1997@gmail.com>
2022-05-12 20:21:01 -0400

    added about me to home page

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "About Me";
 
 document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide";
+
+document.querySelector("body").appendChild(p);
