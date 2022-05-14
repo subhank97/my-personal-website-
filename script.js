379aee5a83dc010e399e4cd7670b53379ad1dd87
Subhan Khan <subhank1997@gmail.com>
2022-05-14 19:30:36 -0400

    changed location for my bio

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "About Me";
+const h1 = document.createElement("h1");
+h1.textContent = "About Me";
 
-document.querySelector("body").appendChild(h2);
+document.querySelector(".about-page").appendChild(h1);
+
 
 const p = document.createElement("p");
-p.textContent = "I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide";
+p.textContent = "I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide.I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide.I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide.";
 
-document.querySelector("body").appendChild(p);
+document.querySelector(".about-page").appendChild(p);
