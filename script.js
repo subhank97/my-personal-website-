df4c9f725919c24c6659a4331a8a64edafe58e46
Subhan Khan <subhank1997@gmail.com>
2022-05-15 21:36:10 -0400

    edited home page and added skill sets

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.querySelector(".about-page").appendChild(h1);
 
 
 const p = document.createElement("p");
-p.textContent = "I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide.I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide.I am outgoing, dedicated, and open-minded. I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time. Currently, I am looking for new career opportunities my current job position cannot provide.";
+p.textContent = "I am a problem solver and strong communicator eager to prove my value and talent. Very motivated to advance and expand my skill set through targeted mentorship and challenging projects.I get across to people and adjust to changes with ease. I believe that a person should work on developing their professional skills and learning new things all the time.";
 
 document.querySelector(".about-page").appendChild(p);
+
+const h2 = document.createElement("h2");
+h2.textContent = "Skill Set";
+
+document.querySelector(".about-page").appendChild(h2);
